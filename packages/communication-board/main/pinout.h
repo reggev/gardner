36da19915e8875c6540a6933233213a56f8a2309
reggev <reggev.am@gmail.com>
2020-06-30 23:40:12 +0300

    corrected pinout

--- packages/communication-board/main/pinout.h
+++ packages/communication-board/main/pinout.h
@@ -1,6 +1,17 @@
 #ifndef _pinout
 #define _pinout
 
-#define D1 5
+// i2c pins are D1 and D2
+// SPI pins are D5, D6, D7, and D8
+
+#define D8 15 // SS
+#define D7 13 // MOSI
+#define D6 12 // MISO
+#define D5 14 // SCK
+#define D4 2
+#define D3 0
+#define D2 4 // SDA
+#define D1 5 // SCL
+#define D0 16
 
 #endif
