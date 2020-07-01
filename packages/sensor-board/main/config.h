36503e0f941a654260fc4a6090cfb4b3f80cc3fd
reggev <reggev.am@gmail.com>
2020-07-02 00:20:12 +0300

    configured the sensors and wire protocol

--- packages/sensor-board/main/config.h
+++ packages/sensor-board/main/config.h
@@ -1,7 +1,13 @@
+#include <arduino.h>
+
 #ifndef __CONFIG__
 #define __CONFIG__
 
-#define SENSOR_PIN A0
+#define SENSOR_PIN_A A0
+#define SENSOR_PIN_B A1
+#define SENSOR_PIN_C A2
+#define SENSOR_PIN_D A3
+
 #define SLAVE_ADDRESS 9
 
 #endif
--- packages/sensor-board/main/main.ino
+++ packages/sensor-board/main/main.ino
@@ -1,13 +1,33 @@
 #include "config.h"
-#include <wire.h>
+#include <Wire.h>
+#include <arduino.h>
+
+int sensors[4] = {SENSOR_PIN_A, SENSOR_PIN_B, SENSOR_PIN_C, SENSOR_PIN_D};
+
+void sample() {
+    int samples[4];
+    for (int ii = 0; ii < 4; ii++) {
+        Serial.print(ii);
+        Serial.print("\t\t\t");
+        samples[ii] = analogRead(sensors[ii]);
+    }
+    Serial.print("\n");
+
+    for (int sample : samples) {
+        Serial.print(sample);
+        Serial.print("\t");
+        Wire.write((byte)sample);
+    }
+    Serial.print("\n");
+}
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(SENSOR_PIN, INPUT);
+    Wire.begin(SLAVE_ADDRESS);
+    Serial.begin(9600);
+    for (int sensorPin : sensors) {
+        pinMode(sensorPin, INPUT);
+    }
+    Wire.onRequest(sample);
 }
 
-void loop() {
-  int value = analogRead(SENSOR_PIN);
-  Serial.println(value);
-  delay(100);
-}
+void loop() {}
