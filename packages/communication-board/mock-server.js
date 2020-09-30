e9c4d248482c7d982eeb5c5786f91e0cfa1ba0fd
reggev <reggev.am@gmail.com>
2020-10-01 02:09:18 +0300

    moved mock-server to express, updated the endpoints

--- packages/communication-board/mock-server.js
+++ packages/communication-board/mock-server.js
@@ -1,19 +1,45 @@
-const jsonServer = require('json-server');
-const app = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults();
+const express = require('express');
+const { json } = require('express');
+const logger = require('morgan');
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(middlewares);
+const app = express();
+app.use(json());
+app.use(logger('dev'));
+
+app.get('/schedule/next-sample', (req, res) => res.json({ minutes: 0.1 }));
 
 app.post('/samples', (req, res) => {
   console.log(req.body);
   return res.json({ minutes: 0.1 });
 });
 
-app.use(router);
+app.get('/', (req, res) => {
+  res.json({
+    links: {
+      nextSample: {
+        href: '/schedule/next-sample',
+        method: 'GET',
+      },
+      schedule: {
+        href: '/schedule',
+        method: 'GET',
+      },
+      postSchedule: {
+        href: '/schedule',
+        method: 'POST',
+      },
+      postSample: {
+        href: '/sample',
+        method: 'POST',
+      },
+      postSamples: {
+        href: '/samples',
+        method: 'POST',
+      },
+    },
+  });
+});
 
 app.listen(PORT, () => {
   console.info(`running on port ${PORT}\n\t press ctrl+c to stop...`);
